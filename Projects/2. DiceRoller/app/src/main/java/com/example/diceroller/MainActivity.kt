c043f7a5e4ba2ccac45723fdda04845cd9e35e87
Kherzie Andal <kherzieandal@gmail.com>
2021-10-19 09:50:39 +0800

    Added Happy Birthday & Dice Roller
    - added the happy birthday project
    - added the dice roller project
    - updated the description

---
+++ Projects/2. DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rollDice()
+        rollDice2()
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            rollDice()
+            rollDice2()
+        }
+    }
+
+    // Class for creating dice and a function for rolling it.
+    class Dice(private val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+
+    // The function of the Rolling Dice result that will be shown once the button was clicked
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+    }
+
+    private fun rollDice2() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView2)
+        val drawableResource = when (diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+        }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# kotlin-android-projects
+# Kotlin Android Projects
 Projects I've created by following the tutorial from Android Developers (developer.android.com)
